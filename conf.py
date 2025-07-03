83b2873b6988ffa05c5ec2168250936e5c0c8a55
wwwplextvlink <plex.tvlinkk@gmail.com>
2025-07-03 15:18:12 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -47,7 +47,7 @@ html_theme_options = {
 raw_enabled = True
 
 # Paths to templates and static files (uncomment if you have any)
-# templates_path = ['_templates']
+templates_path = ['_templates']
 # html_static_path = ['_static']
 
 # Patterns to ignore when looking for source files
